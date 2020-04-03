007cebebea9be46983a54c93ddd40ab137866620
Jack'lul <jacklulcat@gmail.com>
2020-04-03 09:37:21 +0200

    Reword that

--- src/Command/HelpCommand.php
+++ src/Command/HelpCommand.php
@@ -32,7 +32,7 @@ class HelpCommand extends UserCommand
             $text[] = PHP_EOL . '*Reverse image search*:' . PHP_EOL . ' Send any direct image link or photo message';
         } else {
             $text[] = PHP_EOL . '*Random image*:' . PHP_EOL . ' Use  `/random <tags>`  command';
-            $text[] = PHP_EOL . '*Private chat only features were hidden, message me directly to see them.*';
+            $text[] = PHP_EOL . '*Private chat exclusive features were hidden, message me directly to see them.*';
         }
 
         $data = [
